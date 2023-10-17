e55d404ff99bdcbda59cdf5a38e0b20ccb0312e6
JulianaCRodrigues <81633291+JulianaCRodrigues@users.noreply.github.com>
2023-10-17 15:26:08 -0300

    Ajustes listagem de pokemons

---
+++ src/components/typePokemonList/index.js
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+export function TypePokemon({ typePoke, nameType, imageSrc, fnOnClick, activeType }) {
+  const isActive = typePoke === activeType;
+
+  const handleClick = () => {
+    fnOnClick(typePoke)
+  }
+  
+  return (
+    <li>
+      <button className={`type-filter ${typePoke} ${isActive ? "active" : ""} `} onClick={handleClick}>
+        <div className="icon">
+          <Image
+            src={imageSrc}
+            alt=""
+            width={26}
+            height={26}
+          />
+        </div>
+        <span>{nameType}</span>
+      </button>
+    </li>
+  )
+}
